650f870c1a38a2ced464d09c111d8dd4879714c8
laggardkernel <laggardkernel@qq.com>
2018-09-30 23:10:40 +0800

    Chap 03: render forms manually in templates
    - define form class with wtform
    - render forms manually
    - add entry point at the top of pages

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -19,3 +20,9 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
