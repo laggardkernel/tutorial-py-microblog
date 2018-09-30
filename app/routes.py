6d7963b9767e84c6605b60c77a2b4ebcb01168f8
laggardkernel <laggardkernel@qq.com>
2018-09-30 22:16:09 +0800

    Chap 02: template with conditional statement

--- app/routes.py
+++ app/routes.py
@@ -2,10 +2,13 @@
 # @Author: laggardkernel
 # @Date:   2018-08-08 16:29:05
 # @Last Modified by:   laggardkernel
-# @Last Modified time: 2018-08-08 16:29:49
+# @Last Modified time: 2018-08-08 16:56:14
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'laggardkernel'}
+    return render_template('index.html', title='Home', user=user)
