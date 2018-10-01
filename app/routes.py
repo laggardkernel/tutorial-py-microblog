d4496169c8289a3f2e5c2c7ae6f660524fc0a084
laggardkernel <laggardkernel@qq.com>
2018-10-01 08:20:33 +0800

    Chap 03: receive data from post
    - receive posted data
    - get flashed messages in templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from flask import render_template
+from flask import flash, redirect, render_template
 from app import app
 from app.forms import LoginForm
 
@@ -22,7 +22,12 @@ def index():
     return render_template('index.html', title='Home', user=user, posts=posts)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data
+        ))
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
