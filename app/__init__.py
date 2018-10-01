9519d1aabb6559829fb2efb47922912a88aa37a8
laggardkernel <laggardkernel@qq.com>
2018-10-01 09:02:21 +0800

    Chap 04: Define User model, db init

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,12 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)  # access config with app.config
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes
+from app import routes, models
--- config.py
+++ config.py
@@ -2,9 +2,14 @@
 # -*- coding: utf-8 -*-
 import os
 
+basedir = os.path.dirname(os.path.abspath(__file__))
+
 
 class Config(object):
     """
     Store different configs in separate classes
     """
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False # disable signal of db changes
