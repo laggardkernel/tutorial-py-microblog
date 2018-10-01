4c43521f0d72398e03db6058bf190de7a9b454bb
laggardkernel <laggardkernel@qq.com>
2018-10-01 09:30:57 +0800

    Chap 04: inject shell context into flask shell
    - inject context with decorator @app.shell_context_processor
    - integrate IPython as the default shell with flask-shell-ipython

--- microblog.py
+++ microblog.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from app import app
+from app import app, db
+from app.models import User, Post
+
+
+@app.shell_context_processor
+def make_shell_context():
+    """
+    Add additional context into flask shell
+    :return: context dict
+    """
+    return {'db': db, 'User': User, 'Post': Post}
