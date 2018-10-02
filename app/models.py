f1d8d7393423278bbefc87db119e0a976826fb14
laggardkernel <laggardkernel@qq.com>
2018-10-02 08:40:15 +0800

    Chap 05: login user
    - login user with flask-login
    - User model inherits UserMixin from flask-login

--- app/models.py
+++ app/models.py
@@ -2,10 +2,11 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
-from app import db
+from flask_login import UserMixin
+from app import db, login
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -26,6 +27,16 @@ class User(db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+
+@login.user_loader
+def load_user(id):
+    """
+    Convert unique id tracked by Flask-Login into user object from db
+    :param id: id string
+    :return: User model object
+    """
+    return User.query.get(int(id))
 
 
 class Post(db.Model):
