6887deeae1555eb5489af488c35442700ceb80ae
laggardkernel <laggardkernel@qq.com>
2018-10-03 21:10:42 +0800

    Chap 07: sending errors by email
    - test it with python smtp DebuggingServer

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import logging
+from logging.handlers import SMTPHandler
 from flask import Flask
-from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)  # access config with app.config
@@ -12,5 +14,22 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'  # tell flask-login the view handling login
+
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+            credentials=auth, secure=secure
+        )
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
 
 from app import routes, models, errors
--- config.py
+++ config.py
@@ -10,6 +10,14 @@ class Config(object):
     Store different configs in separate classes
     """
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False # disable signal of db changes
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # disable signal of db changes
+
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = os.environ.get('MAIL_PORT')
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = os.environ.get('ADMIN') or ['you-email@example.com']
