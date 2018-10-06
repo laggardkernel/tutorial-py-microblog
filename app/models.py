de5349b345fd3a44c666efdf5920190475cdcaf4
laggardkernel <laggardkernel@qq.com>
2018-10-06 09:32:58 +0800

    Chap 10: add password reset token methods on User model

--- app/models.py
+++ app/models.py
@@ -2,9 +2,11 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
 from hashlib import md5
+from time import time
+import jwt
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-from app import db, login
+from app import app, db, login
 
 followers = db.Table('followers',
                      db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
@@ -68,6 +70,23 @@ class User(UserMixin, db.Model):
             followers.c.follower_id == self.id)
         own = Post.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256'
+        ).decode('utf-8')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            # Expiration Time Claim (exp) is supported by JWT internally
+            # https://pyjwt.readthedocs.io/en/latest/usage.html#expiration-time-claim-exp
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except Exception as e:
+            return
+        return User.query.get(id)
 
 
 @login.user_loader
