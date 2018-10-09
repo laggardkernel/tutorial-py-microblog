a7edcdf69cb2c9ef060654bdfdc2cee60bbf860a
laggardkernel <laggardkernel@gmail.com>
2018-10-09 15:18:21 +0800

    Fix: avoid logging in test mode

--- app/__init__.py
+++ app/__init__.py
@@ -48,7 +48,7 @@ def create_app(config_class=Config):
     from app.main import bp as main
     app.register_blueprint(main)
 
-    if not app.debug:
+    if not app.debug and not app.testing:
         if app.config['MAIL_SERVER']:
             auth = None
             if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
