d109b67d63ee92ed8094e94614ffe70d0b7f45d1
laggardkernel <laggardkernel@gmail.com>
2018-10-09 14:25:19 +0800

    Chap 16: search form and search view

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Practice of Flask Mega Tutorial
 
 [Flask Mega-Tutorial](https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world)
+
+## Additional Settings Needed
+```
+# .env file
+SECRET_KEY=you-may-guess-this
+# export FLASK_DEBUG=1
+
+MAIL_SERVER=localhost
+MAIL_PORT=8025
+MAIL_USE_TLS=1
+MAIL_USERNAME=
+MAIL_PASSWORD=
+ADMIN=admin@example.com
+
+MS_TRANSLATOR_KEY=<paste-your-key-here>
+ELASTICSEARCH_URL='http://localhost:9200'
+```
--- app/main/forms.py
+++ app/main/forms.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from flask import request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Length, ValidationError
@@ -28,3 +29,14 @@ class PostForm(FlaskForm):
         DataRequired(), Length(1, 140)
     ])
     submit = SubmitField(_l('Submit'))
+
+
+class SearchForm(FlaskForm):
+    q = StringField(_l('Search'), validators=[DataRequired()])
+
+    def __init__(self, *args, **kwargs):
+        if 'formdata' not in kwargs:
+            kwargs['formdata'] = request.args
+        if 'csrf_enabled' not in kwargs:
+            kwargs['csrf_enabled'] = False
+        super().__init__(*args, **kwargs)
