c5abbaa6ddadcb43c2215d89024ffb3a9eb387ea
laggardkernel <laggardkernel@gmail.com>
2018-10-10 10:20:15 +0800

    Chap 21: send and receive private messages

--- app/main/routes.py
+++ app/main/routes.py
@@ -6,8 +6,8 @@ from flask_login import current_user, login_required
 from flask_babel import _, get_locale
 from guess_language import guess_language
 from app import db
-from .forms import EditProfileForm, PostForm, SearchForm
-from app.models import User, Post
+from .forms import EditProfileForm, PostForm, SearchForm, MessageForm
+from app.models import User, Post, Message
 from app.translate import translate
 from . import bp
 
@@ -142,9 +142,9 @@ def search():
     page = request.args.get('page', 1, type=int)
     posts, total = Post.search(g.search_form.q.data, page,
                                current_app.config['POSTS_PER_PAGE'])
-    next_url = url_for('main.search', q=g.search_form.q.data, page=page + 1) \
+    next_url = url_for('.search', q=g.search_form.q.data, page=page + 1) \
         if total > page * current_app.config['POSTS_PER_PAGE'] else None
-    prev_url = url_for('main.search', q=g.search_form.q.data, page=page - 1) \
+    prev_url = url_for('.search', q=g.search_form.q.data, page=page - 1) \
         if page > 1 else None
     return render_template('main/search.html', title=_('Search'), posts=posts,
                            next_url=next_url, prev_url=prev_url)
@@ -155,3 +155,34 @@ def search():
 def user_popup(username):
     user = User.query.filter_by(username=username).first_or_404()
     return render_template('main/user_popup.html', user=user)
+
+
+@bp.route('/send_message/<recipient>', methods=['GET', 'POST'])
+@login_required
+def send_message(recipient):
+    user = User.query.filter_by(username=recipient).first_or_404()
+    form = MessageForm()
+    if form.validate_on_submit():
+        msg = Message(author=current_user, recipient=user, body=form.message.data)
+        db.session.add(msg)
+        db.session.commit()
+        flash(_('Your message has been sent.'))
+        return redirect(url_for('.user', username=recipient))
+    return render_template('main/send_message.html', title=_('Send Message'),
+                           form=form, recipient=recipient)
+
+
+@bp.route('/messages')
+@login_required
+def messages():
+    # update last_message_read_time first
+    current_user.last_message_read_time = datetime.utcnow()
+    db.session.commit()
+    page = request.args.get('page', 1, type=int)
+    messages = current_user.messages_received.order_by(
+        Message.timestamp.desc()).paginate(
+        page, current_app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('.messages', page=messages.next_num) if messages.has_next else None
+    prev_url = url_for('.messages', page=messages.prev_num) if messages.has_prev else None
+    return render_template('main/messages.html', messages=messages.items,
+                           next_url=next_url, prev_url=prev_url)
--- app/models.py
+++ app/models.py
@@ -87,6 +87,14 @@ class User(UserMixin, db.Model):
         backref=db.backref('followers', lazy='dynamic'), lazy='dynamic'
     )
 
+    # messages
+    # name the backref as author not sender, to reuse template _posts.html
+    messages_sent = db.relationship('Message', foreign_keys='Message.sender_id',
+                                    backref='author', lazy='dynamic')
+    messages_received = db.relationship('Message', foreign_keys='Message.recipient_id',
+                                        backref='recipient', lazy='dynamic')
+    last_message_read_time = db.Column(db.DateTime)
+
     # followers, people who follow the current user
     # the definitions here are different with those in Table 'followers'
 
@@ -143,6 +151,11 @@ class User(UserMixin, db.Model):
             return
         return User.query.get(id)
 
+    def new_messages(self):
+        last_read_time = self.last_message_read_time or datetime(1900, 1, 1)
+        return Message.query.filter_by(recipient=self).filter(
+            Message.timestamp > last_read_time).count()
+
 
 @login.user_loader
 def load_user(id):
@@ -180,3 +193,14 @@ class Post(SearchableMixin, db.Model):
                 post.language = language
                 db.session.add(post)
                 db.session.commit()
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Message {}>'.format(self.body)
